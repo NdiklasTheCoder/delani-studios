2953e6395ff2bad6b2b6e2a087892541738ee32c
NdiklasTheCoder <nicholas.v.ndiki@gmail.com>
2020-06-16 15:36:40 +0300

    Add contents js script for email form

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let cta = document.getElementById('cta');
+let email = document.getElementById('email');
+let error = document.getElementById('error');
+let success = document.getElementById('success');
+let signup = document.getElementById('signup');
